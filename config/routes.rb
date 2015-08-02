ef2083fa869d80a97a4eed5c70ca9fc22c29f79e
Heeney <heeneysean@gmail.com>
2015-08-02 17:00:40 +0000

    built out navigation and added welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
 
   resources :invoices
   
-  root to: 'welcome#index' <!--- # stands for: controller#Method call , welcome controller, Actual method. --> 
+  root to: 'welcome#index' 
 
 end
